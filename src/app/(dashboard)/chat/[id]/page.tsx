65d11f000c91741a4f9acc39ad6f38583bf15a31
Kisan Kumavat <kisankumavat85@gmail.com>
2026-01-08 11:13:05 +0530

    Add active chat page

--- src/app/(dashboard)/chat/[id]/page.tsx
+++ src/app/(dashboard)/chat/[id]/page.tsx
@@ -1,10 +1,21 @@
+import PromptInput from "@/components/prompt-input";
+
 type Props = { params: Promise<{ id: string }> };
 
 const ActiveChatPage = async (props: Props) => {
   const { params } = props;
   const { id } = await params;
 
-  return <div>Id: {id}</div>;
+  return (
+    <div className="flex flex-col gap-4 border border-red-300 h-full">
+      <div className="flex-1">
+        <h1 className="text-3xl">{id}</h1>
+      </div>
+      <div className="">
+        <PromptInput />
+      </div>
+    </div>
+  );
 };
 
 export default ActiveChatPage;
